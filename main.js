d14205a74f54f70cff514e25ea6adec8311fb2f9
pdoubleu30 <phwonka@gmail.com>
2025-07-06 13:19:28 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,43 @@
+//testing
 
+
+const renderData = (timeframe) => {
+    const section = document.getElementById('updateTime');
+    section.innerHTML = ''; //clears prior content
+    const activity = obj;
+
+    activity.forEach(item =>{
+        const title = item.title;
+        const timeframes = item.timeframes;
+        const current = timeframes[timeframe].current;
+        const previous = timeframes[timeframe].previous;
+    
+
+/*
+    for (const act of activity) { */
+        const myPara1 = document.createElement("p");
+        const myPara2 = document.createElement("p");
+
+        myPara1.textContent = `${current}`;
+        myPara2.textContnt = `Yesterday + '' + '-' +${previous}`;
+
+        /*
+        myPara1.textContent = activity.timeframes.daily.current;
+        myPara2.textContent = `Yesterday +'' +'-' +${activity.timeframes.daily.previous}`;
+        */
+
+
+    section.appendChild(myPara1);
+    section.appendChild(myPara2);
+});
+}
+
+document.querySelectorAll('#buttons button').forEach(button => {
+    button.addEventListener('click', () => {
+        const timeframe = button.id;
+        renderData(timeframe);
+    });
+});
+
+//inital load with data
+renderData('weekly');
